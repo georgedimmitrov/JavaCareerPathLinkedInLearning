30c22cbcb2cd7ffd2381b955521a9dcedd6d85a3
Georgi Dimitrov <dgeorgi@vmware.com>
2019-02-13 23:26:40 +0200

    Creational design patterns chapter 1 - builder.

---
+++ 5.creation-design-patterns/Ch02/02_02/src/simple_builder_example/Architect.java
@@ -0,0 +1,18 @@
+
+package simple_builder_example;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+
+/**
+ *
+ * @author bethan
+ */
+public class Architect {
+    
+    public static void main(String[] args) {
+        Bedroom room = new BedroomBuilder().setDimensions(new Dimension(200, 100)).setCeilingHeight(132).setFloorNumber(2).setWallColor(Color.yellow).setNumberOfWindows(2).setNumberOfDoors(1).setIsDouble(true).setHasEnsuite(true).createBedroom();
+    }
+    
+}
---
+++ 5.creation-design-patterns/Ch02/02_03/src/complete_builder_example/Builder.java
@@ -0,0 +1,17 @@
+package complete_builder_example;
+
+import java.awt.*;
+
+public interface Builder {
+   Builder setDimensions(Dimension dimensions);
+
+   Builder setCeilingHeight(int ceilingHeight);
+
+   Builder setFloorNumber(int floorNumber);
+
+   Builder setWallColor(Color wallColor);
+
+   Builder setNumberOfWindows(int numberOfWindows);
+
+   Builder setNumberOfDoors(int numberOfDoors);
+}
